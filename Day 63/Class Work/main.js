6d126137016adff2745dea2c79a605c27539e66f
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2026-03-08 21:12:51 +0400

    Update main.js

--- Day 63/Class Work/main.js
+++ Day 63/Class Work/main.js
@@ -2,6 +2,10 @@ const moves = ["rock", "paper", "scissors"];
 
 let user = prompt("Enter Your Move (Rock, Paper, Scissors):").toLowerCase();
 
+while (user !== "rock" && user !== "paper" && user !== "scissors") {
+    user = prompt("Please Enter a Valid Move (Rock, Paper, Scissors):").toLowerCase();
+}
+
 function random(arr) {
     const randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
@@ -11,15 +15,13 @@ const computer = random(moves);
 
 console.log("Computer:", computer);
 
-if (user === "rock" && computer === "paper") {
-    alert("You Lose");
-}
-else if (user === "scissors" && computer === "paper") {
-    alert("You Win");
-}
-else if (user === "rock" && computer === "scissors") {
-    alert("You Win");
-}
-else if (user === computer) {
+if (user === computer) {
     alert("It's a Tie");
 }
+else if ( user === "rock" && computer === "scissors" || user === "paper" && computer === "rock" || user === "scissors" && computer === "paper") {
+    alert("You Win");
+}
+else {
+    alert("You Lose");
+}
+
