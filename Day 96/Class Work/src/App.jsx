e61683bafb589fc5597fae2018d9407f2305a4d3
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2026-05-21 18:33:33 +0400

    main

---
+++ Day 96/Class Work/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+    
+
+    <main>
+
+      <div className="whiteDiv">
+        <div className="maindiv">
+            <img src="/image-qr-code.png" alt="" />
+
+            <h2>Improve your front-end skills by building projects</h2>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </div>
+    </main>
+    
+    
+    </>
+  )
+}
+
+export default App
