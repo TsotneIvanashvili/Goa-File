efae63a9a5e33c91bffd10ae28b5586206404b4b
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2026-05-24 22:39:25 +0400

    main

--- Day 95/Home Work/main.js
+++ Day 95/Home Work/main.js
@@ -1,7 +1,21 @@
 import { getStorage } from "./utils/localstorage.js";
 
-
+const main = document.querySelector("main")
 const user = getStorage("CurrentUser")
+const div = document.createElement("div")
 
+function renderUser(){
+    if(user !== null){
+        div.innerHTML = ` 
+        <h1>${user.fName}</h1>
+        
+        `
+        main.appendChild(div)
+    }
+    else{
+        console.error("A");
+        
+    }
+}
 
-console.log(user);
+renderUser()
